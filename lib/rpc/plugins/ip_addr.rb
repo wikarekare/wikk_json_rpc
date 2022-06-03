fce7263c4329ee325dfa15b3fe26990616aecb58
robertburrowes <r.burrowes@auckland.ac.nz>
2022-06-03 15:05:35 +1200

    convention is to use kwargs

--- lib/rpc/plugins/ip_addr.rb
+++ lib/rpc/plugins/ip_addr.rb
@@ -7,7 +7,7 @@ class IP_Addr < RPC
   end
 
   # rmethods are the RPC methods we call via the JSON RPC
-  rmethod :ip_addr do |select_on: nil, set: nil, result: nil, **_args| # rubocop: disable Lint/UnusedBlockArgument # Want consistent params
+  rmethod :ip_addr do |select_on: nil, set: nil, result: nil, **_kwargs| # rubocop: disable Lint/UnusedBlockArgument # Want consistent params
     return response(address_string: @requestor)
   end
 
