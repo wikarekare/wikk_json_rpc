90a0eb375ea01d7c16cf90cb1f2b44009fe902ac
robertburrowes <r.burrowes@auckland.ac.nz>
2022-06-24 12:15:01 +1200

    added a test to see what rmethods actually get registered

---
+++ rlib/rpc/plugins/test.rb
@@ -0,0 +1,26 @@
+# Return message sent to us
+class Symbol
+  def to_j
+    self.to_s.to_j
+  end
+end
+
+# Respond with a list of rmethod calls
+class Test < RPC
+  def initialize(authenticated = false)
+    super(authenticated)
+    @requestor = ENV['REMOTE_ADDR']
+    @messages = ''
+  end
+
+  # rmethods are the RPC methods we call via the JSON RPC
+  rmethod :get_rmethods do |select_on: nil, set: nil, result: nil, **kwargs| # rubocop: disable Lint/UnusedBlockArgument # Want consistent params
+    return response(rmethods: @@rmethods)
+  end
+
+  private def response(rmethods:)
+    return { 'rmethods' => rmethods,
+             'messages' => @messages
+            }
+  end
+end
---
+++ rlib/rpc/test.rb
@@ -0,0 +1,52 @@
+#!/usr/local/bin/ruby
+require 'json'
+require '/wikk/rlib/wikk_conf.rb'
+require 'wikk_json'
+require 'wikk_webbrowser'
+
+# Set up to call the webserver via the loopback
+class RPC
+  def initialize(url:, identity: nil, auth: nil)
+    @cookies = []
+    @url = url
+    @identity = identity
+    @auth = auth
+  end
+
+  def self.rpc(url:, query:, identity: nil, auth: nil) # rubocop:disable Lint/UnusedMethodArgument Want the args to match initialize
+    WIKK::WebBrowser.http_session(host: '127.0.0.1') do |ws|
+      response = ws.post_page(query: url,
+                              content_type: 'application/json',
+                              data: query.to_j
+                             )
+      return JSON.parse(response)
+    end
+  end
+end
+
+# Call the test plugin to get a list of rmethods
+def rmethods
+  begin
+    r = RPC.rpc( query: { 'method' => 'Test.get_rmethods',
+                          'kwparams' => {
+                            'select_on' => {},
+                            'set' => {},
+                            'result' => []
+                          },
+                          'id' => "#{Time.now.to_i}",
+                          'version' => '1.1'
+                        },
+                 url: '/ruby/rpc.rbx'
+               )
+    return if r.nil?
+
+    r['result']['rmethods'].each do |the_class, the_rmethods|
+      puts "#{the_class} #{the_rmethods}"
+    end
+    puts r['result']['messages']
+  rescue StandardError => e
+    puts e.message
+  end
+end
+
+rmethods
