e7ddc2ea1dc115e6925d04cec72c0faa8e6a4f01
robertburrowes <r.burrowes@auckland.ac.nz>
2023-03-27 16:48:27 +1300

    Added CGI::Session to be compatible with the old login.rbx cgi.
    passed barebones CGI class to wikk_authenticate, and had to change the 
    pstore location away from tmp, as apache2 puts tmp into a private 
    namespace.

--- test/thin_server.rb
+++ test/thin_server.rb
@@ -15,6 +15,44 @@ require_relative "#{RLIB}/rpc/rpc.rb"
 # The advantage is, that this process doesn't need to run as Apache, so can have a different Apparmor profile.
 # Class needs a call() method for Thin.
 class Responder
+  # Stripped down CGI class, with just the cookies
+  class Minimal_CGI
+    attr_accessor :cookies
+    attr_accessor :output_cookies
+    attr_accessor :remote_addr
+
+    def initialize(env:)
+      @env = env
+      @cookies = {}
+      @remote_addr = @env['HTTP_X_FORWARDED_FOR'].nil? ? @env['REMOTE_ADDR'] : @env['HTTP_X_FORWARDED_FOR']
+      cookie_string = env['HTTP_COOKIE']
+      unless cookie_string.nil?
+        @key_values = cookie_string.split(';')
+        @key_values.each do |kv|
+          t = kv.strip.split('=', 2) # Split on first =
+          @cookies[t[0]] = t[1] unless t.nil? || t.length != 2
+        end
+      end
+      @output_cookies = []  # We get this back from CGI::Session
+      @output_hidden = {}   # We get this back from CGI::Session, which we ignore
+    end
+
+    # Don't think this is the behaviour of CGI, but it will do.
+    def [](_the_key)
+      nil
+    end
+
+    # Don't think this is the behaviour of CGI, but it will do.
+    def key?(_the_key)
+      false
+    end
+
+    # Convert each cookie to a string, and return the resulting array
+    def cookies_to_a
+      @output_cookies.map(& :to_s)
+    end
+  end
+
   # Init the Responder class
   def initialize(debug: false)
     @response = 'the quick brown fox<br>' # Test string response.
@@ -27,12 +65,13 @@ class Responder
     @env = env
     @req = Rack::Request.new(env)
     @the_body = @req.body.read     # We will want the json, and can't read this twice (debug and extract)
+    @cgi = Minimal_CGI.new(env: env)
 
     dump_html if @debug
 
     if @message.nil?
       # We could be behind a forwarding proxy server, so we will not see the ENV that we need to.
-      ENV['REMOTE_ADDR'] = @env['HTTP_X_FORWARDED_FOR'].nil? ? @env['REMOTE_ADDR'] : @env['HTTP_X_FORWARDED_FOR']
+      ENV['REMOTE_ADDR'] = @cgi.remote_addr
       # Might be using REST methodology, where the REQUEST_METHOD alters behaviour
       ENV['REQUEST_METHOD'] = @env['REQUEST_METHOD']
       # simple_test_pattern
@@ -44,6 +83,10 @@ class Responder
       return [ 500, { 'Content-Type' => 'application/json' }, [ @message.to_j ]]
     end
   end
+
+  # For our testing, we want CGI::Session and Rack to work together
+  # CGI::Session takes a cgi instance argument, and injects an @output_cookies[CGI::Cookies] array
+  # There are calls to cgi.key?(session_key), cgi[session_key] failing over to request.cookies[session_key]
 
   # Turn json in query body into a Hash. We expect it to be json
   private def extract_json
@@ -95,14 +138,16 @@ class Responder
       puts rpc_json if @debug
       response = RPC.rpc( authenticated: authenticated?, query: rpc_json )
 
-      headers = {
-        'Content-Type' => 'application/json',
-        'Set-Cookie' => [ 'foo=bar', 'bar=foo' ]
-      }
+      headers = { 'Content-Type' => 'application/json' }
+      # Cookies come back to us in env['HTTP_COOKIE'] as a string, and go back as 'Set-Cookie'
+      # inject session cookies into the response ( @cgi update by authenticated?() )
+      headers['Set-Cookie'] = @cgi.cookies_to_a unless @cgi.output_cookies.empty?
 
       return [ 200, headers, [ response ]]
     rescue Exception => e # rubocop: disable Lint/RescueException # We need to return to the caller, and not just crash
+      warn e.message
       backtrace = e.backtrace[0].split(':')
+      warn backtrace
       @message = "MSG: (#{File.basename(backtrace[-3])} #{backtrace[-2]}): #{e.message.to_s.gsub(/'/, '\\\'')}".gsub(/\n/, ' ').gsub(/</, '&lt;').gsub(/>/, '&gt;')
       response = { code: -32000,
                    response: @message,
@@ -125,6 +170,13 @@ class Responder
     puts "rack_input = #{rack_input_str}"
     # rack_errors_str = @env['rack.errors'].read
     # puts "rack_errors = #{rack_errors_str}"
+    puts "\nCookies IN"
+    @cgi.cookies.each do |k, c|
+      puts "#{k} => #{c}"
+    end
+    puts "\nCookies OUT"
+    p @cgi.cookies_to_a
+    puts "\nAuthenticated? #{authenticated?}"
     puts "\nEnv:"
     @env.each do |k, v|
       puts "  #{k} = #{v}"
