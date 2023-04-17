ced8cf30544fd58e26b8958c659640f2f4cf0d33
robertburrowes <r.burrowes@auckland.ac.nz>
2023-04-17 15:42:26 +1200

    A test of calling a nonexistent method

---
+++ test/fail.rb
@@ -0,0 +1,39 @@
+#!/usr/local/bin/ruby
+require 'time'
+RLIB = '/wikk/rlib' unless defined? RLIB
+require_relative "#{RLIB}/wikk_conf.rb"  # to get MYSQL_CONF (which we don't actually use, but is read)
+require_relative '../rlib/rpc/rpc.rb'
+require_relative '../rlib/rpc/minimal_cgi.rb'
+
+# Test through Ruby RPC instance, not via TCP Socket
+def test_rpc_echo(cgi)
+  # rpc = RPC.new
+  puts 'Entering test_rpc_echo'
+  begin
+    r = RPC.rpc(  cgi: cgi,
+                  query: { 'method' => 'RPC_Echo.no_method',
+                           'kwparams' => {
+                             'select_on' => { 'message' => 'The quick brown fox jumped over the lazy dog' },
+                             'set' => {},
+                             'result' => []
+                           },
+                           'id' => 1234,
+                           'version' => '1.1'
+                        },
+                  authenticated: true
+               )
+    puts 'test_rpc_echo: RPC_Echo.echo call completed'
+    puts r.class
+    puts r
+  rescue StandardError => e
+    puts e.message
+  end
+end
+
+env = {
+  'REMOTE_ADDR' => '127.0.0.1',
+  'HTTP_COOKIE' => ''
+}
+cgi = WIKK::Minimal_CGI.new(env: env)
+
+test_rpc_echo(cgi)
