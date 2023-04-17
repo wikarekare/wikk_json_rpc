7c08a7b55e5f15d4c891ed53c2f64d437ee72944
robertburrowes <r.burrowes@auckland.ac.nz>
2023-04-17 13:43:17 +1200

    Use Minimal_CGI class for direct test calls to RPC plugins

--- test/local_test.rb
+++ test/local_test.rb
@@ -3,22 +3,24 @@ require 'time'
 RLIB = '/wikk/rlib' unless defined? RLIB
 require_relative "#{RLIB}/wikk_conf.rb"  # to get MYSQL_CONF (which we don't actually use, but is read)
 require_relative "#{RLIB}/rpc/rpc.rb"
+require_relative "#{RLIB}/rpc/minimal_cgi.rb"
 
 # Test through Ruby RPC instance, not via TCP Socket
-def test_rpc_echo
+def test_rpc_echo(cgi)
   # rpc = RPC.new
   puts 'Entering test_rpc_echo'
   begin
-    r = RPC.rpc( query: { 'method' => 'RPC_Echo.echo',
-                          'kwparams' => {
-                            'select_on' => { 'message' => 'The quick brown fox jumped over the lazy dog' },
-                            'set' => {},
-                            'result' => []
-                          },
-                          'id' => 1234,
-                          'version' => '1.1'
+    r = RPC.rpc(  cgi: cgi,
+                  query: { 'method' => 'RPC_Echo.echo',
+                           'kwparams' => {
+                             'select_on' => { 'message' => 'The quick brown fox jumped over the lazy dog' },
+                             'set' => {},
+                             'result' => []
+                           },
+                           'id' => 1234,
+                           'version' => '1.1'
                         },
-                 authenticated: true
+                  authenticated: true
                )
     puts 'test_rpc_echo: RPC_Echo.echo call completed'
     puts r.class
@@ -28,4 +30,10 @@ def test_rpc_echo
   end
 end
 
-test_rpc_echo
+env = {
+  'REMOTE_ADDR' => '127.0.0.1',
+  'HTTP_COOKIE' => ''
+}
+cgi = WIKK::Minimal_CGI.new(env: env)
+
+test_rpc_echo(cgi)
--- test/traffic_summary_test.rb
+++ test/traffic_summary_test.rb
@@ -3,13 +3,15 @@ require 'time'
 require 'json'
 RLIB = '/wikk/rlib' unless defined? RLIB
 require_relative "#{RLIB}/rpc/rpc.rb"
+require_relative "#{RLIB}/rpc/minimal_cgi.rb"
 
 # Test through Ruby RPC instance, not via TCP Socket
-def test_rpc_rmethods
+def test_rpc_rmethods(cgi)
   # rpc = RPC.new
   puts 'Entering test_rpc_echo'
   begin
-    r = RPC.rpc( query: { 'method' => 'Traffic.site_daily_usage_summary',
+    r = RPC.rpc( cgi: cgi,
+                 query: { 'method' => 'Traffic.site_daily_usage_summary',
                           'kwparams' => {
                             'select_on' => { 'hostname' => 'wikk003' },
                             'set' => {},
@@ -27,5 +29,11 @@ def test_rpc_rmethods
   end
 end
 
+env = {
+  'REMOTE_ADDR' => '127.0.0.1',
+  'HTTP_COOKIE' => ''
+}
+cgi = WIKK::Minimal_CGI.new(env: env)
+
 puts 'Daily Traffic Summary Site wikk003'
-test_rpc_rmethods
+test_rpc_rmethods(cgi)
